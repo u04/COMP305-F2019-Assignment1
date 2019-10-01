39d782581d4af293037b5ba4122bbcd8c3eac163
u04 <email@email.com>
2019-10-01 18:42:46 -0400

    have moving background

---
+++ Assets/_Scripts/BackgroundController.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundController : MonoBehaviour
+{
+    public float verticalSpeed = 0.1f;
+    public float resetPosition = 3.9f;
+    public float resetPoint = -4.08f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Reset();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+        CheckBounds();
+    }
+    /// <summary>
+    /// Moves background down
+    /// </summary>
+    void Move()
+    {
+        Vector2 newPositition = new Vector2(0.0f, verticalSpeed);
+        Vector2 currentPosition = transform.position;
+
+        currentPosition -= newPositition;
+        transform.position = currentPosition;
+    }
+    /// <summary>
+    /// Moves background to reset position
+    /// </summary>
+    void Reset()
+    {
+        transform.position = new Vector2(0.0f, resetPosition);
+    }
+
+    void CheckBounds()
+    {
+        if (transform.position.y <= resetPoint)
+        {
+            Reset();
+        }
+    }
+}
