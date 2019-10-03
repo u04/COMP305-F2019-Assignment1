aa16b50fc45a7a20e3fa9df88773c1355c2a127f
u04 <email@email.com>
2019-10-03 19:52:33 -0400

    added collision detection and some sound

---
+++ Assets/_Scripts/PlayerController.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float maxSpeed =0.001f ;
+    public float minSpeed =0.001f ;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+        CheckBounds();
+    }
+    public void Move()
+    {
+        Vector2 newPosition = transform.position;
+        if(Input.GetAxis("Horizontal") > 0.0f)
+        {
+            newPosition += new Vector2(maxSpeed, 0.0f);
+        }
+        if (Input.GetAxis("Horizontal") < 0.0f)
+        {
+            newPosition += new Vector2(minSpeed, 0.0f);
+        }
+        transform.position = newPosition;
+    }
+    public void CheckBounds()
+    {
+        if(transform.position.x > 8.16f)
+        {
+            transform.position = new Vector2(8.16f, transform.position.y);
+        }
+
+        if (transform.position.x < -8.1f)
+        {
+            transform.position = new Vector2(-8.1f, transform.position.y);
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        switch (collision.gameObject.tag)
+        {
+            case "Planet":
+                Debug.Log("hit planet");
+                break;
+            case "Health":
+                Debug.Log("hit health");
+                break;
+        }
+    }
+}
